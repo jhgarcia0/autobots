246df928cb1b37e53f136dbcc551a6b890a35150
BuildTools <joao.henrique.dev@gmai.com>
2023-05-25 14:25:01 -0300

    finalizando cadastro de veiculos e o get de veiculos, falta a validacao de veiculos e proprietarios

--- back-end/src/main/java/unit/caruru/autobots/Controller/ProprietarioController.java
+++ back-end/src/main/java/unit/caruru/autobots/Controller/ProprietarioController.java
@@ -1,7 +1,7 @@
 package unit.caruru.autobots.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import unit.caruru.autobots.Service.ProprietariosService;
 import unit.caruru.autobots.Model.Proprietario;
 
 import java.time.LocalDate;
@@ -10,8 +10,9 @@ import java.util.List;
 import java.util.Objects;
 
 @RestController
-public class CnhController {
-    private List<Proprietario> proprietarios = new ArrayList<>();
+public class ProprietarioController {
+
+    public List<Proprietario> proprietarios = new ArrayList<>();
     @GetMapping("/cadastrar/cnh")
     private void inserirCnh(@RequestParam(name="nome") String nome, @RequestParam(name="cpf") String cpf,
                             @RequestParam(name="categoria")String categoria, @RequestParam(name="validade") LocalDate validade){
@@ -20,7 +21,7 @@ public class CnhController {
     }
 
     @GetMapping("/cnh/{cpf}")
-    private Proprietario getCnhByCpf(@PathVariable("cpf") String cpf){
+    public Proprietario getCnhByCpf(@PathVariable("cpf") String cpf){
         for (int i=0; i<proprietarios.size(); i++) {
             if (Objects.equals(proprietarios.get(i).getCpf(), cpf)){
                 return proprietarios.get(i);
