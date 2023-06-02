6a93ced8896d7235d0a51aa8ec07dd8058f2c797
Fredy Prudente <thenomefredy@hotmail.com>
2023-06-01 22:13:01 -0300

    100 Feito

--- back-end/src/main/java/unit/caruru/autobots/Controller/veiculoController.java
+++ back-end/src/main/java/unit/caruru/autobots/Controller/veiculoController.java
@@ -165,9 +165,9 @@ public class veiculoController {
     }
     }
     @PostMapping("/transferencia-veiculo")
-    public ResponseEntity<String> transferirVeiculo(@RequestParam(name = "inputPlaca") String placa,
-                                                    @RequestParam(name = "inputAntigoProprietario") String antigoProprietarioCpf,
-                                                    @RequestParam(name = "inputCPFProprietario") String novoProprietarioCpf)   throws ProprietarioNotFoundException, VeiculoNotFoundException {
+    public RedirectView transferirVeiculo(@RequestParam(name = "inputPlaca") String placa,
+                                          @RequestParam(name = "inputAntigoProprietario") String antigoProprietarioCpf,
+                                          @RequestParam(name = "inputCPFProprietario") String novoProprietarioCpf) throws ProprietarioNotFoundException, VeiculoNotFoundException {
 
         Veiculo veiculo = getVeiculoByPlaca(placa);
         Proprietario antigoProprietario = veiculo.getProprietario();
@@ -177,12 +177,11 @@ public class veiculoController {
         Proprietario novoProprietario = cnhController.getCnhByCpf(novoProprietarioCpf);
 
         veiculo.setProprietario(novoProprietario);
+
+        return new RedirectView("/transfer.html");
     
-        return ResponseEntity.ok().build();
+
     }
-    
-
-
 
     
 }
